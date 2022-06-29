892d53e69cae48276a91eb6b7e3249c0fb8c4243
Nebulizer1213 <fixingg@gmail.com>
2022-06-29 07:46:15 -0400

    update

---
+++ KeyboardSoundPlayer GUI old/KeyboardSoundPlayer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace KeyboardSoundPlayer
+{
+    static class Program
+    {
+        private static readonly Mutex Mutex = new Mutex(true, "KeyboardSoundPlayerGUI");
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            if (Mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Application.EnableVisualStyles();
+                Application.SetHighDpiMode(HighDpiMode.SystemAware);
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            else { return; }
+        }
+    }
+}
